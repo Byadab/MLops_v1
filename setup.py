808ea43267482b9f01909cdbf9f29ca27d332f92
Binay Yadab <Binay3344@gmail.com>
2024-07-18 19:45:41 +0530

    Setup and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+#building the application as package itself
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+
+#metadata infor of project
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Binay',
+    author_email = 'binay3344@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
